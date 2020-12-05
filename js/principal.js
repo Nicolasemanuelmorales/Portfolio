2e7e2c0799d3e4ba39dc5ed0ba9a43a3276297f3
Nicolás Emanuel Morales <nicolasemanuelmorales@hotmail.com>
2020-12-05 00:02:44 -0300

    arreglos

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,6 @@
 function cambiarAOscuro() {
-  setCookie("Claro","false");
-  setCookie("Oscuro","true");
+  setCookie("Claro", "false");
+  setCookie("Oscuro", "true");
   $("hr").addClass("hr");
   document.getElementById("body").classList.remove('w3-light-grey');
   document.getElementById("body").classList.add('negro-oscuro');
@@ -30,8 +30,8 @@ function cambiarAOscuro() {
 }
 
 function cambiarAClaro() {
-  setCookie("Oscuro","false");
-  setCookie("Claro","true");
+  setCookie("Oscuro", "false");
+  setCookie("Claro", "true");
   $("hr").removeClass("hr");
   document.getElementById("body").classList.add('w3-light-grey');
   document.getElementById("body").classList.remove('negro-oscuro');
@@ -60,20 +60,20 @@ function cambiarAClaro() {
   document.getElementById("footer").classList.remove('blanco-falso');
 }
 
-function CambiarEstado(){
-  if(document.getElementById("myonoffswitch").checked === true){
+function CambiarEstado() {
+  if (document.getElementById("myonoffswitch").checked === true) {
     cambiarAOscuro();
-  }else{cambiarAClaro();}
+  } else { cambiarAClaro(); }
 }
 
-function CambiarEstadoIdioma(){
-  if(document.getElementById("myonoffswitch2").checked === true){
-    setCookie("Arg","false");
-    setCookie("En","true");
-   }else{
-    setCookie("En","false");
-    setCookie("Arg","true");
-   }
+function CambiarEstadoIdioma() {
+  if (document.getElementById("myonoffswitch2").checked === true) {
+    setCookie("Arg", "false");
+    setCookie("En", "true");
+  } else {
+    setCookie("En", "false");
+    setCookie("Arg", "true");
+  }
 }
 
 function getCookie(cname) {
@@ -92,7 +92,7 @@ function getCookie(cname) {
   return "";
 }
 var localStorage2 = window.localStorage;
-function setCookie(cname,cvalue) {
+function setCookie(cname, cvalue) {
   localStorage2.setItem(cname, cvalue);
 }
 
@@ -101,129 +101,130 @@ function cargaDeCookies() {
   var Claro = "";
   var Arg = "";
   var En = "";
-  Oscuro= localStorage2.getItem("Oscuro");
-  Claro= localStorage2.getItem("Claro");
-  Arg= localStorage2.getItem("Arg");
-  En= localStorage2.getItem("En");
+  Oscuro = localStorage2.getItem("Oscuro");
+  Claro = localStorage2.getItem("Claro");
+  Arg = localStorage2.getItem("Arg");
+  En = localStorage2.getItem("En");
 
-  if(Arg == "true"){
-    $('.lang').each(function(index, item) {
+  if (Arg == "true") {
+    $('.lang').each(function (index, item) {
       $(this).text(arrLang["en"][$(this).attr('key')]);
-    });  
+    });
     document.getElementById("myonoffswitch2").checked = false;
   }
-  else{
-    $('.lang').each(function(index, item) {
+  else {
+    $('.lang').each(function (index, item) {
       $(this).text(arrLang["arg"][$(this).attr('key')]);
-    });    
+    });
     document.getElementById("myonoffswitch2").checked = true;
   }
 
-  if(Claro == "true"){
+  if (Claro == "true") {
     document.getElementById("myonoffswitch").checked = false;
     cambiarAClaro();
   }
-  else{
+  else {
     cambiarAOscuro();
     document.getElementById("myonoffswitch").checked = true;
   }
 }
 
 
- $(window).resize(function () {
-      if($(window).width() > 600){
-      var h = document.getElementById("der").clientHeight;
-      $("#cuadro-izq").css({ minHeight: h+"px" });
-    }
-    else{
-      $("#cuadro-izq").css({ minHeight:"" });
-    }
-    });
+$(window).resize(function () {
+  if ($(window).width() > 600) {
+    var h = document.getElementById("der").clientHeight;
+    $("#cuadro-izq").css({ minHeight: h + "px" });
+  }
+  else {
+    $("#cuadro-izq").css({ minHeight: "" });
+  }
+});
 
-    $(document).ready(function() {
-      if($(window).width() > 600){
-      var h = document.getElementById("der").clientHeight;
-      $("#cuadro-izq").css({ minHeight: h+"px" });
-      }
-      else{
-        $("#cuadro-izq").css({ minHeight:"" });
-      }
-    });
+$(document).ready(function () {
+  if ($(window).width() > 600) {
+    var h = document.getElementById("der").clientHeight;
+    $("#cuadro-izq").css({ minHeight: h + "px" });
+  }
+  else {
+    $("#cuadro-izq").css({ minHeight: "" });
+  }
+});
 
-$('#myonoffswitch2').change(function() {
-        if($(this).is(":checked")) {
-          $('.lang').each(function(index, item) {
-          $(this).text(arrLang["arg"][$(this).attr('key')]);
-        });        }else{
-          $('.lang').each(function(index, item) {
-          $(this).text(arrLang["en"][$(this).attr('key')]);
-        });
-        }
+$('#myonoffswitch2').change(function () {
+  if ($(this).is(":checked")) {
+    $('.lang').each(function (index, item) {
+      $(this).text(arrLang["arg"][$(this).attr('key')]);
     });
+  } else {
+    $('.lang').each(function (index, item) {
+      $(this).text(arrLang["en"][$(this).attr('key')]);
+    });
+  }
+});
 
-    var arrLang = {
-      'en': {
-        'ExperienciaTitulo': 'Work experience',
-        'Agosto': 'August',
-        'Actualmente': 'Nowadays',
-        'puesto2': 'Customer Service',
-        'descPuesto2P1': 'Online procedures - Loans - Sale of reloadable debit cards - Money transfers to the interior and exterior of the country - Ticket sales - Upload system - Photocopier management - Cash and stock closing - Cash monitoring and control in Rapipago sector.',
-        'descPuesto2P2': 'Approximately 1 year in a shift manager position, in charge of 5 people.',
-        'descPuesto3': 'Technical support in a call center on the 3 services provided by the Telecentro company (Internet, Telephony, TV).',
-        'Abril': 'April',
-        'Octubre': 'October',
-        'Noviembre': 'November ',
-        'Enero': 'January',
-        'Educacion': 'Education',
-        'EnCurso': 'In progress',
-        'Tec': 'University Technician in Web Development',
-        'Redes': 'Computer network installer',
-        'Pc': 'PC repair',
-        'Bachiller': 'Bachelor with an orientation in Economics & Administration',
-        'Proyectos': 'Projects',
-        'Proyecto1T': 'About 3 Months',
-        'Proyecto1Inf': 'Ecommerce - Forum with AI - University Project',
-        'Proyecto1': ' Helping others',
-        'Proyecto2T': 'About 2 Months',
-        'Proyecto2Inf': 'Donations Network - University Project',
-        'Proyecto3T': 'About 1 Month',
-        'Proyecto3Inf': 'Ecommerce - University Project',
-        'Footer':'Find me on my social networks.',
-        'Idioma': 'Language',
-        'Español':'Spanish',
-        'Ingles': 'English',
-        'St':'Technical support | Call center - Contactcom',
-      },
-      'arg': {
-        'ExperienciaTitulo': 'Experiencia laboral',
-        'Agosto': 'Agosto',
-        'Actualmente': 'Actualmente',
-        'puesto2': 'Servicio de atención al cliente',
-        'descPuesto2P1': 'Trámites online - Prestamos - Venta de tarjetas de debito recargables - Giros al interior y exterior del pais - Venta de pasajes - Sistema de sube - Manejo de fotocopiadora - Cierre de caja y stock - Seguimiento y control de caja en sector Rapipago.',
-        'descPuesto2P2': 'Aproximadamente un 1 año en puesto de encargado de turno, a cargo de 5 personas.',
-        'descPuesto3': 'Soporte técnico en un call center sobre los 3 servicios que brinda la empresa Telecentro (Internet, Telefonía, TV).',
-        'Abril': 'Abril',
-        'Octubre': 'Octubre',
-        'Noviembre': 'Noviembre',
-        'Enero': 'Enero',
-        'Educacion': 'Educación',
-        'EnCurso': 'En Curso',
-        'Tec': 'Técnico Universitario en Desarrollo Web',
-        'Redes': 'Instalador de redes informaticas',
-        'Pc': 'Reparación de PC',
-        'Bachiller': 'Bachiller con orientación en Economía & Administración',
-        'Proyectos': 'Proyectos',
-        'Proyecto1T': ' Aproximadamente 3 Meses',
-        'Proyecto1Inf': 'Ecommerce - Foro con IA',
-        'Proyecto1': 'Ayudando al projimo',
-        'Proyecto2T': ' Aproximadamente 2 Meses',
-        'Proyecto2Inf': 'Red de donaciones',
-        'Proyecto3T': ' Aproximadamente 1 Mes',
-        'Proyecto3Inf': 'Ecommerce',
-        'Footer':'Buscame en mis redes sociales.',
-        'Idioma': 'Idioma',
-        'Español':'Español',
-        'Ingles': 'Inglés',
-        'St':'Soporte técnico | Call center - Contactcom',
-      }
-    };
+var arrLang = {
+  'en': {
+    'ExperienciaTitulo': 'Work experience',
+    'Agosto': 'August',
+    'Actualmente': 'Nowadays',
+    'puesto2': 'Customer Service',
+    'descPuesto2P1': 'Online procedures - Loans - Sale of reloadable debit cards - Money transfers to the interior and exterior of the country - Ticket sales - Upload system - Photocopier management - Cash and stock closing - Cash monitoring and control in Rapipago sector.',
+    'descPuesto2P2': 'Approximately 1 year in a shift manager position, in charge of 5 people.',
+    'descPuesto3': 'Technical support in a call center on the 3 services provided by the Telecentro company (Internet, Telephony, TV).',
+    'Abril': 'April',
+    'Octubre': 'October',
+    'Noviembre': 'November ',
+    'Enero': 'January',
+    'Educacion': 'Education',
+    'EnCurso': 'In progress',
+    'Tec': 'University Technician in Web Development',
+    'Redes': 'Computer network installer',
+    'Pc': 'PC repair',
+    'Bachiller': 'Bachelor with an orientation in Economics & Administration',
+    'Proyectos': 'Projects',
+    'Proyecto1T': 'About 3 Months',
+    'Proyecto1Inf': 'Ecommerce - Forum with AI - University Project',
+    'Proyecto1': ' Helping others',
+    'Proyecto2T': 'About 2 Months',
+    'Proyecto2Inf': 'Donations Network - University Project',
+    'Proyecto3T': 'About 1 Month',
+    'Proyecto3Inf': 'Ecommerce - University Project',
+    'Footer': 'Find me on my social networks.',
+    'Idioma': 'Language',
+    'Español': 'Spanish',
+    'Ingles': 'English',
+    'St': 'Technical support | Call center',
+  },
+  'arg': {
+    'ExperienciaTitulo': 'Experiencia laboral',
+    'Agosto': 'Agosto',
+    'Actualmente': 'Actualmente',
+    'puesto2': 'Servicio de atención al cliente',
+    'descPuesto2P1': 'Trámites online - Prestamos - Venta de tarjetas de debito recargables - Giros al interior y exterior del pais - Venta de pasajes - Sistema de sube - Manejo de fotocopiadora - Cierre de caja y stock - Seguimiento y control de caja en sector Rapipago.',
+    'descPuesto2P2': 'Aproximadamente un 1 año en puesto de encargado de turno, a cargo de 5 personas.',
+    'descPuesto3': 'Soporte técnico en un call center sobre los 3 servicios que brinda la empresa Telecentro (Internet, Telefonía, TV).',
+    'Abril': 'Abril',
+    'Octubre': 'Octubre',
+    'Noviembre': 'Noviembre',
+    'Enero': 'Enero',
+    'Educacion': 'Educación',
+    'EnCurso': 'En Curso',
+    'Tec': 'Técnico Universitario en Desarrollo Web',
+    'Redes': 'Instalador de redes informaticas',
+    'Pc': 'Reparación de PC',
+    'Bachiller': 'Bachiller con orientación en Economía & Administración',
+    'Proyectos': 'Proyectos',
+    'Proyecto1T': ' Aproximadamente 3 Meses',
+    'Proyecto1Inf': 'Ecommerce - Foro con IA',
+    'Proyecto1': 'Ayudando al projimo',
+    'Proyecto2T': ' Aproximadamente 2 Meses',
+    'Proyecto2Inf': 'Red de donaciones',
+    'Proyecto3T': ' Aproximadamente 1 Mes',
+    'Proyecto3Inf': 'Ecommerce',
+    'Footer': 'Buscame en mis redes sociales.',
+    'Idioma': 'Idioma',
+    'Español': 'Español',
+    'Ingles': 'Inglés',
+    'St': 'Soporte técnico | Call center',
+  }
+};
